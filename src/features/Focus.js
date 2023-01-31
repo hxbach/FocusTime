98194d9fa5916ba0bdb9fdfd07aa4d6ea8a929ff
hxbach <bachboong@gmail.com>
2023-01-31 19:55:51 +0700

    Hi

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -13,7 +13,7 @@ export const Focus = ({ addSubject }) => {
         <TextInput
           style={styles.textInput}
           onChangeText={setSubject}
-          label="Nay định tập trung làm gì nào? 123"
+          label="Nay định tập trung làm gì nào? 1234"
         />
         <View style={styles.button}>
           <RoundedButton
