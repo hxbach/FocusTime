a4b5b82b406d9cf66ed4f96038d2175e1ba60116
hxbach <bachboong@gmail.com>
2023-01-31 19:52:45 +0700

    fis

---
+++ src/features/Focus.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { RoundedButton } from '../Components/RoundedButton';
+import { color } from '../utils/colors';
+import { spacing } from '../utils/sizes';
+
+export const Focus = ({ addSubject }) => {
+  const [subject, setSubject] = useState(null);
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setSubject}
+          label="Nay định tập trung làm gì nào?"
+        />
+        <View style={styles.button}>
+          <RoundedButton
+            title="+"
+            size={50}
+            onPress={() => addSubject(subject)}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+  },
+  button: {
+    justifyContent: 'center',
+  },
+
+  textInput: {
+    flex: 1,
+    marginRight: spacing.sm,
+  },
+  inputContainer: {
+    padding: spacing.lg,
+    justifyContent: 'top',
+    flexDirection: 'row',
+  },
+});
